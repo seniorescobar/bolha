9d2887cc9320313def350970eac01e24e8a19afc
Aleksandar <aleksandarjjj@icloud.com>
2019-07-10 14:07:28 +0200

    lambda client work in progress

---
+++ lambda-client/go.mod
@@ -0,0 +1,8 @@
+module github.com/seniorescobar/bolha/lambda-client
+
+go 1.12
+
+require (
+	github.com/aws/aws-lambda-go v1.11.1
+	github.com/seniorescobar/bolha/client v0.0.0-20190710094735-0abec475f553
+)
---
+++ lambda-client/main.go
@@ -0,0 +1,76 @@
+package main
+
+import (
+	"context"
+	"encoding/json"
+
+	"github.com/aws/aws-lambda-go/events"
+	"github.com/aws/aws-lambda-go/lambda"
+	"github.com/seniorescobar/bolha/client"
+)
+
+const (
+	qURL = "https://sqs.eu-central-1.amazonaws.com/301808156345/bolha-ads-queue"
+
+	actionUpload = "upload"
+	actionRemove = "remove"
+)
+
+type Ad struct {
+	Title       string `json:"title"`
+	Description string `json:"description"`
+	Price       int    `json:"price"`
+	CategoryId  int    `json:"category-id"`
+}
+
+func Handler(ctx context.Context, event events.SQSEvent) error {
+	for _, record := range event.Records {
+		action, ok := record.MessageAttributes["action"]
+		if !ok {
+			return nil
+		}
+
+		username, ok := record.MessageAttributes["username"]
+		if !ok {
+			return nil
+		}
+
+		password, ok := record.MessageAttributes["password"]
+		if !ok {
+			return nil
+		}
+
+		c, err := client.New(&client.User{
+			Username: *username.StringValue,
+			Password: *password.StringValue,
+		})
+
+		switch *action.StringValue {
+		case actionUpload:
+			var ad Ad
+			if err := json.Unmarshal(record.Body, &ad); err != nil {
+				return err
+			}
+
+			_, err := uploadAd(c, &ad)
+			if err != nil {
+				return err
+			}
+		}
+	}
+
+	return nil
+}
+
+func uploadAd(c *client.Client, ad *Ad) (int64, error) {
+	return c.UploadAd(&client.Ad{
+		Title:       ad.Title,
+		Description: ad.Description,
+		Price:       ad.Price,
+		CategoryId:  ad.CategoryId,
+	})
+}
+
+func main() {
+	lambda.Start(Handler)
+}
