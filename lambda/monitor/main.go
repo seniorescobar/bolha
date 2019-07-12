1457a702ff222c5f451123308d5ed4bce2289f81
Aleksandar <aleksandarjjj@icloud.com>
2019-07-12 19:25:29 +0200

    calling functions from common

--- lambda/monitor/main.go
+++ lambda/monitor/main.go
@@ -2,176 +2,98 @@ package main
 
 import (
 	"context"
-	"encoding/json"
-	"strconv"
 
 	"github.com/aws/aws-lambda-go/lambda"
-	"github.com/aws/aws-sdk-go/aws"
 	"github.com/aws/aws-sdk-go/aws/session"
-	"github.com/aws/aws-sdk-go/service/sqs"
 
 	"github.com/seniorescobar/bolha/client"
 	"github.com/seniorescobar/bolha/db/postgres"
-
-	log "github.com/sirupsen/logrus"
+	"github.com/seniorescobar/bolha/lambda/common"
 )
 
 const (
 	allowedOrder = 3
-
-	qURL = "https://sqs.eu-central-1.amazonaws.com/301808156345/bolha-ads-queue"
-
-	actionUpload = "upload"
-	actionRemove = "remove"
 )
 
-type Record struct {
-	User *User
-	Ad   *Ad
-}
-
-type User struct {
-	Username string
-	Password string
-}
-
-type Ad struct {
-	Id          int64    `json:"id"`
-	Title       string   `json:"title"`
-	Description string   `json:"description"`
-	Price       int      `json:"price"`
-	CategoryId  int      `json:"category-id"`
-	Images      []string `json:"images"`
-}
-
-func GetActiveAds(ctx context.Context) ([]*client.ActiveAd, error) {
-	sess, err := session.NewSession()
-	if err != nil {
-		return nil, err
-	}
-
-	svc := sqs.New(sess)
-
+func Handler(ctx context.Context) error {
 	pdb, err := postgres.NewFromEnv()
 	if err != nil {
-		return nil, err
+		return err
 	}
 	defer pdb.Close()
 
-	users, err := pdb.ListActiveUsers(ctx)
+	sess := session.Must(session.NewSession())
+
+	sqsClient, err := common.NewSQSClient(sess)
 	if err != nil {
-		return nil, err
+		return err
 	}
 
-	outdatedAds := make([]*client.ActiveAd, 0)
+	users, err := pdb.ListActiveUsers(ctx)
+	if err != nil {
+		return err
+	}
+
 	for _, user := range users {
 		c, err := client.New(&client.User{
 			Username: user.Username,
 			Password: user.Password,
 		})
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		activeAds, err := c.GetActiveAds()
 		if err != nil {
-			return nil, err
+			return err
 		}
 
 		for _, activeAd := range activeAds {
-			if activeAd.Order > allowedOrder {
-				outdatedAds = append(outdatedAds, activeAd)
-
+			// check re-upload condition
+			if reUploadCondition(activeAd.Order) {
 				record, err := pdb.GetRecord(ctx, activeAd.Id)
 				if err != nil {
-					return nil, err
+					return err
 				}
 
-				log.Printf("record %+v\n", record)
-
-				if err := sendRemoveMessage(svc, &User{
+				// send remove message
+				if err := sqsClient.SendRemoveMessage(&common.User{
 					Username: record.User.Username,
 					Password: record.User.Password,
 				}, activeAd.Id); err != nil {
-					return nil, err
+					return err
 				}
 
-				if err := sendUploadMessage(svc, &Record{
-					User: &User{
-						Username: record.User.Username,
-						Password: record.User.Password,
-					},
-					Ad: &Ad{
-						Id:          record.Ad.Id,
-						Title:       record.Ad.Title,
-						Description: record.Ad.Description,
-						Price:       record.Ad.Price,
-						CategoryId:  record.Ad.CategoryId,
-						Images:      record.Ad.Images,
-					},
-				}); err != nil {
-					return nil, err
+				// send upload message
+				if err := sqsClient.SendUploadMessage(&common.User{
+					Username: record.User.Username,
+					Password: record.User.Password,
+				}, &common.Ad{
+					Id:          record.Ad.Id,
+					Title:       record.Ad.Title,
+					Description: record.Ad.Description,
+					Price:       record.Ad.Price,
+					CategoryId:  record.Ad.CategoryId,
+					Images:      record.Ad.Images,
+				},
+				); err != nil {
+					return err
 				}
 			}
 		}
 	}
 
-	return outdatedAds, nil
+	return nil
 }
 
-func sendUploadMessage(svc *sqs.SQS, record *Record) error {
-	adJSON, err := json.Marshal(record.Ad)
-	if err != nil {
-		return err
+func reUploadCondition(order int) bool {
+	if order > allowedOrder {
+		return true
 	}
 
-	log.Info("sending ad JSON", string(adJSON))
-
-	_, err = svc.SendMessage(&sqs.SendMessageInput{
-		MessageAttributes: map[string]*sqs.MessageAttributeValue{
-			"action": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String(actionUpload),
-			},
-			"username": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String(record.User.Username),
-			},
-			"password": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String(record.User.Password),
-			},
-		},
-		MessageBody: aws.String(string(adJSON)),
-		QueueUrl:    aws.String(qURL),
-	})
-
-	return err
-}
-
-func sendRemoveMessage(svc *sqs.SQS, user *User, uploadedAdId int64) error {
-	_, err := svc.SendMessage(&sqs.SendMessageInput{
-		MessageAttributes: map[string]*sqs.MessageAttributeValue{
-			"action": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String(actionRemove),
-			},
-			"username": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String(user.Username),
-			},
-			"password": &sqs.MessageAttributeValue{
-				DataType:    aws.String("String"),
-				StringValue: aws.String(user.Password),
-			},
-		},
-		MessageBody: aws.String(strconv.FormatInt(uploadedAdId, 10)),
-		QueueUrl:    aws.String(qURL),
-	})
-
-	return err
+	return false
 }
 
 func main() {
-	lambda.Start(GetActiveAds)
+	lambda.Start(Handler)
 }
